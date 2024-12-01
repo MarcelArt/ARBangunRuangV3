ad2aba6182fd6e6f01ac9fdc8cdd5f0b2c54055c
BabangGen <vito.putra28@gmail.com>
2024-12-02 00:20:17 +0700

    update scripct

--- Assets/Scripts/TargetHandlerLPDtr.cs
+++ Assets/Scripts/TargetHandlerLPDtr.cs
@@ -14,30 +14,21 @@ public class TargetHandlerLPDtr : MonoBehaviour
 
     void Update()
     {
-        // Periksa apakah target Kubus aktif
-        if (kubusTarget.activeInHierarchy)
-        {
-            ShowKubusInput();
-        }
-        // Periksa apakah target Balok aktif
-        else if (LimasTarget.activeInHierarchy)
-        {
-            ShowBalokInput();
-        }
-        // Jika tidak ada target yang aktif
-        else
-        {
-            ResetFields();
-        }
+    
     }
 
-    void ShowKubusInput()
+    private void Start()
+    {
+        ResetFields();
+    }
+
+    public void ShowKubusInput()
     {
         ResetFields();
         inputFieldA.SetActive(true); // Hanya aktifkan inputFieldA
     }
 
-    void ShowBalokInput()
+    public void ShowBalokInput()
     {
         ResetFields();
         inputFieldB.SetActive(true);
@@ -45,7 +36,7 @@ public class TargetHandlerLPDtr : MonoBehaviour
         inputFieldD.SetActive(true);
     }
 
-    void ResetFields()
+    public void ResetFields()
     {
         inputFieldA.SetActive(false);
         inputFieldB.SetActive(false);
